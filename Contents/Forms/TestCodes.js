5ba5c6ef5833cb13934ac72ac8ca779f564f5a03
rmnauman <rmnauman@gmail.com>
2024-11-03 12:16:45 +0500

    Login Form Complete

--- Contents/Forms/TestCodes.js
+++ Contents/Forms/TestCodes.js
@@ -19,25 +19,65 @@ const TestCodes = () => {
         }
     });
 
-    const [userName, setUserName] = useState('hello');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [messageColor, setMessageColor] = useState('red');
+
+    var AllData = [
+        { uName: "GPT", pass: "123" },
+        { uName: "Faiz", pass: "000" },
+        { uName: "Samandur Khan", pass: "1122" },
+    ];
+
+    const onPressButton = () => {
+        if (!userName || password.length == 0) {
+            setMessage('Please Enter All Values');
+        }
+        else {
+            var ind = -1;
+            var ans = AllData.filter(val => val.uName == userName && val.pass == password)
+            // for (var i = 0; i < AllData.length; i++) {
+            //     if (userName == AllData[i].uName && password == AllData[i].pass) {
+            //         ind = i;
+            //         break;
+            //     }
+            // }
+            if (ans.length > 0) {
+                setMessage('Correct Data');
+                setMessageColor('green');
+            }
+            else {
+                setMessage('In Correct Data');
+                setMessageColor('red');
+            }
+        }
+    }
     return (
         <View style={ss.mainView}>
             <View style={ss.viewInputs}>
                 <TextInput
-                    onChangeText={val => { setUserName(val) }}
+                    onChangeText={(val) => { setUserName(val) }}
+                    placeholder="Enter User Name"
+                    placeholderTextColor='purple'
                     style={ss.txtInputs}></TextInput>
-                <TextInput style={ss.txtInputs}></TextInput>
+                <TextInput
+                    onChangeText={setPassword}
+                    placeholder="Enter Password"
+                    style={ss.txtInputs}></TextInput>
             </View>
             <View>
                 <Text
                     style={{
                         textAlign: 'center',
-                        fontSize: 35, color: 'black'
+                        fontSize: 35, color: messageColor,
                     }}>
-                    {userName}</Text>
+                    {message}</Text>
             </View>
-            <View style={{ alignSelf: 'center' }}>
-                <Button title="Login"></Button>
+            <View style={{ alignSelf: 'center', width: '50%' }}>
+                <Button
+                    onPress={onPressButton}
+                    title="Login"></Button>
             </View>
         </View>
     );
